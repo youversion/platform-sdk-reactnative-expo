58ed9407a4785c419663ac88d8c5736fc622b36b
Cameron Pak <cameronandrewpak@gmail.com>
2026-05-04 19:51:07 -0500

    fix: export BibleCard from public API

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,3 @@
-export { VerseOfTheDay, FootnoteContent, BibleReaderDOM, BibleTextViewDOM } from './dom'
+export { BibleCard, VerseOfTheDay, FootnoteContent, BibleReaderDOM, BibleTextViewDOM } from './dom'
 export { BibleReader, BibleTextView, NativeSheet, NativeSheetProvider } from './native'
 export type { FootnoteData } from '@youversion/platform-react-ui'
